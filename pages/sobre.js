e94828a267168ea70882b1ab96bcedf40faf60d9
B <lucasdcbarbosa2@gmail.com>
2022-02-08 12:57:19 -0300

    testee

--- pages/sobre.js
+++ pages/sobre.js
@@ -4,7 +4,7 @@ function SOBRE(){
     return (<div>
         <h1>SOBRE CXLXMBIV KIDS</h1>
         <link href="/">
-             <a>Acessar página Sobre</a>
+             <a>Acessar página home</a>
         </link>
      </div>)
 }
